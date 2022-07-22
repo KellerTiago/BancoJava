c263d2fd5b6b72077e9aa2da4a824dfa8a18f64a
KellerTiago <kellertiago@hotmail.com>
2022-07-21 22:19:51 -0300

    Estudo principio do polimorfismo

--- Teste.java
+++ Teste.java
@@ -4,11 +4,25 @@ public class Teste {
 	
 	public static void main(String[] args) {
 	
-		ContaCorrente cc = new ContaCorrente ();
-		cc.setAgencia(1111);
-		cc.setNumero(2222);
-		cc.setTipo("PF");
-		cc.setCheckEspecial(500);
+		Conta conta1 = new Conta ();
+		conta1.depositar(1000);
+		conta1.retirar(100);
+		
+		ContaCorrente conta2 = new ContaCorrente ();
+		conta2.depositar(1000);
+		conta2.retirar(100);
+		
+		Conta conta3 = new ContaCorrente ();
+		conta3.depositar(1000);
+		conta3.retirar(100);
+		
+		System.out.println("Saldo a partir da SuperClass " + conta1.getSaldo());
+		System.out.println("Saldo a partir da SubClass " + conta2.getSaldo());
+		System.out.println("Saldo a partir da SuperClass com objeto da SubClass pelo metodo Overide " + conta3.getSaldo());
+		
+		
+		
+		
 		
 		
 		
